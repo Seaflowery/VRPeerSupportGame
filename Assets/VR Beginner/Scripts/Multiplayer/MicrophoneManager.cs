eed9745a635643bfc86299555bf654765b9dfb7e
Seaflowery <12012940@mail.sustech.edu.cn>
2023-06-12 15:54:59 +0800

    microphone manager

--- Assets/VR Beginner/Scripts/Multiplayer/MicrophoneManager.cs
+++ Assets/VR Beginner/Scripts/Multiplayer/MicrophoneManager.cs
@@ -1,62 +1,73 @@
 ﻿using UnityEngine;
 using Mirror;
+using System.IO;
+using System;
 
 public class MicrophoneManager : NetworkBehaviour
 {
-    const int FREQUENCY = 44100;
-    AudioClip mic;
-    int lastPos, pos;
-    
-    void Start()
+    private AudioClip audioClip;
+
+    [Command]
+    private void CmdSendVoiceData(byte[] audioData)
     {
-        mic = Microphone.Start(null, true, 1, FREQUENCY);
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.loop = true;
-        // audio.clip = mic;
-        while (!(Microphone.GetPosition(null) > 0)) { }
-        audio.Play();
+        RpcReceiveVoiceData(audioData);
     }
 
-    
-    void Update()
+    [ClientRpc]
+    private void RpcReceiveVoiceData(byte[] audioData)
     {
-        pos = Microphone.GetPosition(null);
-        if (pos > lastPos)
+        // Convert received audio data to audio clip
+        AudioClip receivedAudioClip = ConvertByteArrayToAudioClip(audioData);
+
+        // Play the received audio clip
+        AudioSource.PlayClipAtPoint(receivedAudioClip, Vector3.zero);
+    }
+
+    private void Start()
+    {
+        // Initialize the microphone
+        if (Microphone.devices.Length > 0)
         {
-            if (!isServer)
-                Log.Instance.CmdLog("innnnnnnnnnnnnnn!");
-            float[] samples = new float[(pos - lastPos) * mic.channels];
-            mic.GetData(samples, lastPos);
-            AudioSource audioSource = GetComponent<AudioSource>();
-            
-            // Debug.Log(audio.clip);
-            audioSource.clip = AudioClip.Create("MicrophoneClip", samples.Length, mic.channels, FREQUENCY, false);
-            audioSource.clip.SetData(samples, 0);
-    
-            // audio.clip.SetData(samples, lastPos);
-            if (!isServer)
-            {
-                Log.Instance.CmdLog("qwq!");
-                CmdSendAudio(samples);
-            }
+            string deviceName = Microphone.devices[0];
+            audioClip = Microphone.Start(deviceName, true, 10, AudioSettings.outputSampleRate);
         }
-        lastPos = pos;
+        else
+        {
+            Debug.LogError("No microphone found!");
+        }
     }
-    
-    [Command(requiresAuthority = false)]
-    void CmdSendAudio(float[] samples)
+
+    private void Update()
     {
-        Debug.Log("sending audio");
-        RpcReceiveAudio(samples);
+        if (isLocalPlayer && Microphone.IsRecording(null))
+        {
+            // Get the microphone data and convert it to a byte array
+            float[] samples = new float[audioClip.samples];
+            audioClip.GetData(samples, 0);
+
+            byte[] audioData = ConvertAudioClipToByteArray(samples);
+
+            // Send the audio data to the server to be relayed to other clients
+            CmdSendVoiceData(audioData);
+        }
     }
-    
-    [ClientRpc(includeOwner = false)]
-    void RpcReceiveAudio(float[] samples)
+
+    private byte[] ConvertAudioClipToByteArray(float[] samples)
     {
-        AudioSource audio = GetComponent<AudioSource>();
-        // audio.clip.SetData(samples, 0);
-        // audio.Play();
-        Log.Instance.CmdLog("audio received");
+        int length = samples.Length * 4;
+        byte[] byteArray = new byte[length];
+        Buffer.BlockCopy(samples, 0, byteArray, 0, length);
+        return byteArray;
     }
-    
+
+    private AudioClip ConvertByteArrayToAudioClip(byte[] byteArray)
+    {
+        int length = byteArray.Length / 4;
+        float[] samples = new float[length];
+        Buffer.BlockCopy(byteArray, 0, samples, 0, byteArray.Length);
+
+        AudioClip audioClip = AudioClip.Create("ReceivedAudioClip", length, 1, AudioSettings.outputSampleRate, false);
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
 }
